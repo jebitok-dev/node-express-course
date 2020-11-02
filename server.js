b12cce766f99a7591dbbb6153c76c4237cc88717
Jebitok <sharonkosgei4@gmail.com>
2020-11-02 19:27:06 +0300

    added first GET route

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express'); //gives access to express server
 const app = express();
 
+//mock JSON data
+const mockUserData = [
+    {name: 'Mark'},
+    {name: 'Ann'}
+]
+app.get('/users', function(req, rse){
+    resizeBy.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: mockUserData
+    })
+})
 
 //app.listen method starts the server locally on localhost:8000
-app.listen(8000, function() {
-    console.log("server is running")
-})
+app.listen(8000, function() {console.log("server is running")})
